1b57a47a42f0b6c0fbf68d04da4146447ae5feac
abhi4gitt <raiabhishek631@gmail.com>
2025-12-28 19:34:05 +0530

    Initial Commit

---
+++ LOTRCurrencyConverter/LOTRCurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRCurrencyConverterApp.swift
+//  LOTRCurrencyConverter
+//
+//  Created by Abhishek on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRCurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
