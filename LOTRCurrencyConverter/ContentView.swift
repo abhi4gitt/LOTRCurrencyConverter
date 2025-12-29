ad4c3cba85f4a508fb8e1a4a6e46b87321414f57
abhi4gitt <raiabhishek631@gmail.com>
2025-12-29 06:27:15 +0530

    Add image assets and create the main screen of the app

--- LOTRCurrencyConverter/ContentView.swift
+++ LOTRCurrencyConverter/ContentView.swift
@@ -9,13 +9,85 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            // Background Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                // Prancing Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                // Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    
+                    // Left conversion section
+                    VStack {
+                        
+                        // Currency
+                        HStack {
+                            
+                            // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        // Text field
+                        Text("Text field")
+                    }
+                    
+                    // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    // Right conversion section
+                    VStack {
+                        
+                        // Currency
+                        HStack {
+                            
+                            // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            // Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        
+                        // Text field
+                        Text("Text field")
+                    }
+                }
+                
+                // Info Button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            }
         }
-        .padding()
     }
 }
 
