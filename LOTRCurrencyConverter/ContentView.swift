edd456d805f0441d7fa80c443ab210e985291924
abhi4gitt <raiabhishek631@gmail.com>
2025-12-30 14:05:28 +0530

    Add convert function and attached it to the UI

--- LOTRCurrencyConverter/ContentView.swift
+++ LOTRCurrencyConverter/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     
     @State var showSelectCurrency = false
     
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
     @State var leftCurrency = Currency.silverPiece
     @State var rightCurrency: Currency = .goldPiece
     
@@ -64,6 +67,12 @@ struct ContentView: View {
                         // Text field
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
+                            .focused($leftTyping)
+                            .onChange(of: leftAmount) {
+                                if leftTyping {
+                                    rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+                                }
+                            }
                     }
                     
                     // Equal Sign
@@ -95,10 +104,15 @@ struct ContentView: View {
                         }
                         
                         // Text field
-                        TextField("Amount", text: $leftAmount)
+                        TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
-                        
+                            .focused($rightTyping)
+                            .onChange(of: rightAmount) {
+                                if rightTyping {
+                                    leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                                }
+                            }
                     }
                 }
                 .padding()
